62f5decffaa2e88ff9fe9c21d651e297e73bd093
Franklin Ikeh <ikehfranklin06@gmail.com>
2026-03-25 16:40:05 +0100

    refactor: Restructure components and update imports for consistency

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -11,13 +11,13 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <PresidentialWelcome />
-      <About />
-      <Departments />
-      <Executives />
-      <Responsibilities />
+      {/* <PresidentialWelcome /> */}
+      {/* <About /> */}
+      {/* <Departments /> */}
+      {/* <Executives /> */}
+      {/* <Responsibilities /> */}
       <News />
-      <Contact />
+      {/* <Contact /> */}
     </>
   );
 }
