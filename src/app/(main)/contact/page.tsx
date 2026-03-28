4d3a730372409f2e8f22d25b95a76b7fc2366c7e
Franklin Ikeh <ikehfranklin06@gmail.com>
2026-03-28 10:55:26 +0100

    feat: Update page title for Contact page

--- src/app/(main)/contact/page.tsx
+++ src/app/(main)/contact/page.tsx
@@ -3,7 +3,7 @@ import Hero from "./_components/Hero";
 import ContactInfo from "./_components/ContactInfo";
 
 export const metadata: Metadata = {
-	title: "Contact | CISSA",
+	title: "Contact",
 	description:
 		"Get in touch with the Communication and Information Sciences Students Association, University of Ilorin.",
 	keywords: [
